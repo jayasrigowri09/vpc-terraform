dadf211fa4c29b54a3eb2ae12381e1b0e65a11f9
Jayasrigowri09 <235503@ust-global.com>
2023-03-16 14:53:32 -0400

    add

---
+++ vpc/outputs.tf
@@ -0,0 +1,14 @@
+output "vpc_arn" {
+    description = "The ARN of the VPC"
+    value = ""
+}
+
+output "vpc_id" {
+    description = "The Id of the VPC"
+    value = ""
+}
+
+output "route_table_id" {
+    description = "The ID of the route table"
+    value = ""
+}
