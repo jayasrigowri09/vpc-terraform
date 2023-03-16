68c1caa9b74a6c3e4b5daafa30c14e896d92a93c
Jayasrigowri09 <235503@ust-global.com>
2023-03-16 19:26:34 -0400

    add

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,6 +1,6 @@
-output "vpc_arn" {
-    description = "The ARN of the VPC"
-    value = ""
+#output "vpc_arn" {
+ #   description = "The ARN of the VPC"
+  #  value = ""
 }
 
 output "vpc_id" {
@@ -12,7 +12,7 @@ output "route_table_id" {
     description = "The ID of the route table"
     value = "aws_route_table.route_table.id"
 }
-output "subnet_id" {
-    description = "The ID of the subnet"
-    value = "aws_subnet.test_vpc.id"
+#output "subnet_id" {
+ #   description = "The ID of the subnet"
+  #  value = "aws_subnet.test_vpc.id"
 }
