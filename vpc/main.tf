2b1c1ef6bfb5974c64ede98717fce416b5abcbc4
Jayasrigowri09 <235503@ust-global.com>
2023-03-16 14:33:51 -0400

    add

---
+++ vpc/main.tf
@@ -0,0 +1,56 @@
+
+
+resource "aws_vpc" "test_vpc" {
+ cidr_block = "10.0.0.0/16"
+ 
+ tags = {
+   Name = "Project-VPC"
+ }
+}
+
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.test_vpc.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Public Subnet ${count.index + 1}"
+ }
+}
+ 
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.test_vpc.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Private Subnet ${count.index + 1}"
+ }
+}
+
+resource "aws_internet_gateway" "Igw" {
+ vpc_id = aws_vpc.test_vpc.id
+ 
+ tags = {
+   Name = "Project VPC IG"
+ }
+}
+
+resource "aws_route_table" "route_table" {
+ vpc_id = aws_vpc.test_vpc.id
+ 
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.Igw.id
+ }
+ 
+ tags = {
+   Name = "Route-Table"
+ }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+ count = length(var.public_subnet_cidrs)
+ subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+ route_table_id = aws_route_table.route_.id
+}
