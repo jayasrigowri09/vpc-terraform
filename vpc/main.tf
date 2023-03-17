2e01de607896caedf6d6b860c8a57422f3d40ac6
Jayasrigowri09 <235503@ust-global.com>
2023-03-16 21:16:04 -0400

    add

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,6 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
+
+
 
 resource "aws_vpc" "test_vpc" {
  cidr_block = "10.0.0.0/16"
