48d7e57037d4eecc5d368008d62b252909d3d689
Jayasrigowri09 <235503@ust-global.com>
2023-03-16 21:10:15 -0400

    add

--- vpc/main.tf
+++ vpc/main.tf
@@ -57,5 +57,5 @@ resource "aws_route_table" "route_table" {
 resource "aws_route_table_association" "public_subnet_asso" {
  count = length(var.public_subnet_cidrs)
  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
- route_table_id = aws_route_table.route_.id
+ route_table_id = aws_route_table.route_table.id
 }
